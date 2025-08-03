37e173fb31d836793357e88e67eb2fe30089209a
bio test <biotest053@gmail.com>
2025-08-04 00:45:32 +0200

    add base64 decoding

--- Plugin/java/com/decacagle/commands/ReadCurrentMetadataCommand.java
+++ Plugin/java/com/decacagle/commands/ReadCurrentMetadataCommand.java
@@ -12,19 +12,41 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Base64;
 import java.util.logging.Level;
 
 public class ReadCurrentMetadataCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        sender.sendMessage(ChatColor.GREEN + "Reading metadata.. ");
 
-        String metadata = APIManager.getMetadata((Player)sender, sender);
-        sender.sendMessage(metadata);
+        if (args.length == 0) {
+            sender.sendMessage(ChatColor.GREEN + "Reading metadata.. ");
 
-        DecaDB.instance.getLogger().log(Level.INFO, metadata);
+            String metadata = APIManager.getMetadata((Player)sender, sender);
+            sender.sendMessage(metadata);
 
+            DecaDB.instance.getLogger().log(Level.INFO, metadata);
+        }
+        else
+        {
+            boolean base64decode = Boolean.parseBoolean(args[0]);
+
+            String metadata1 = APIManager.getMetadata((Player)sender, sender);
+            if (base64decode) {
+                byte[] decoded = Base64.getDecoder().decode(metadata1);
+                String metadata = new String(decoded);
+                sender.sendMessage(metadata);
+
+                DecaDB.instance.getLogger().log(Level.INFO, metadata);
+            }
+            else
+            {
+                sender.sendMessage(metadata1);
+
+                DecaDB.instance.getLogger().log(Level.INFO, metadata1);
+            }
+        }
         return true;
     }
 }
